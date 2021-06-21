1997f89d3bb05bfb35b76a500856daa90793d0a2
Sashmu <sasmithanagesh@gmail.com>
2021-06-21 18:04:35 +0200

    homepage and restaurant layout

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 3,
-   spaceBetween: 30,
+   breakpoints: {
+   
+      // when window width is > 320px but < 768px  
+      320: {
+         slidesPerView: 1,
+         spaceBetween: 50
+      },
+      // when window width is < 1200px but > 768px
+      768: {
+         slidesPerView: 2,
+         spaceBetween: 50
+      },
+  
+      // when window width is > 1200px     
+      1200: {
+         slidesPerView: 3,
+         spaceBetween: 50
+      },
+          // when window width is > 1200px     
+          1400: {
+            slidesPerView: 4,
+            spaceBetween: 50
+         }
+   } ,
+   // spaceBetween: 30,
    autoplay: {
      delay: 2000,
    },
-   pagination: {
-     el: ".swiper-pagination",
-     clickable: true,
-   },
- });
+
+});
+ 
+const form = document.getElementById('myform');
+form.addEventListener('submit', function (event) {
+const alert=document.querySelector('.alert')
+   //event.stopImmediatePropagation();
+   event.preventDefault();
+   event.preventDefault();
+  alert.classList.remove("disappear");
+ alert.classList.add("block");
+   });
